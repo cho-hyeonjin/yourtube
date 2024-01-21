fb725c9d815945f81964a561a58cdb4a3f494628
cho-hyeonjin <gusls2005@gmail.com>
2024-01-21 13:02:14 +0900

    feat: router 정의

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter } from "react-router-dom";
+import NotFound from "./pages/NotFound.jsx";
+import VideoDetail from "./pages/VideoDetail.jsx";
+import Videos from "./pages/Videos.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Videos /> },
+      { path: "videos", element: <Videos /> },
+      { path: "videos/:keyword", element: <Videos /> },
+      { path: "videos/watch/:videoId", element: <VideoDetail /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
