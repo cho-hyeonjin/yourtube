67c7439c027a5f5bec2679fd9a34794e1a2ecb03
cho-hyeonjin <gusls2005@gmail.com>
2024-01-21 17:00:55 +0900

    css: SearchHeader 스타일링

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -14,19 +14,22 @@ export default function SearchHeader() {
   useEffect(() => setText(keyword || ""), [keyword]); // 경로가 이동될 때마다 parameter로 들어온 text value를 검색창 state에도 반영
   return (
     <>
-      <header className="flex">
-        <Link to="/">
-          <BsYoutube />
-          <h1>YourTube</h1>
+      <header className="w-full flex p-4 text-xl border-b border-zinc-600 mb-4">
+        <Link to="/" className="flex items-center">
+          <BsYoutube className="text-5xl text-brand" />
+          <h1 className="font-bold ml-2 text-3xl">
+            YourTube<span className="text-zinc-400 font-thin">ᴷᴿ</span>
+          </h1>
         </Link>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="w-full flex justify-center">
           <input
             type="text"
             placeholder="검색"
             value={text}
             onChange={(e) => setText(e.target.value)}
+            className="w-7/12 p-2 pl-6 outline-none bg-black text-gray-50 rounded-l-full border-y border-l border-zinc-500"
           />
-          <button>
+          <button className="bg-zinc-600 px-6 rounded-r-full border border-zinc-500">
             <BsSearch />
           </button>
         </form>
