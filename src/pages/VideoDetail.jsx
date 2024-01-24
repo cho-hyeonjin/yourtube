1beca4ef8770ba9a1fcd5e8eb5c09e8eb3422427
cho-hyeonjin <gusls2005@gmail.com>
2024-01-24 10:43:05 +0900

    VideoDetail 컴포넌트 iframe 적용 - Video에서 클릭한 아이템 UI에 들어오게

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,7 +1,26 @@
+import { useLocation } from "react-router-dom";
+
 export default function VideoDetail() {
+  const {
+    state: { video },
+  } = useLocation();
+  const { title } = video.snippet;
+
   return (
     <>
-      <div>VideoDetail</div>
+      <section>
+        <iframe
+          id="player"
+          type="text/html"
+          width="100%"
+          height="720"
+          src={`http://www.youtube.com/embed/${video.id}`}
+          className="rounded-2xl"
+        />
+        <div>
+          <h2>{title}</h2>
+        </div>
+      </section>
     </>
   );
 }
