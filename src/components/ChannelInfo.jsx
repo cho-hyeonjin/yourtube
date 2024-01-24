f86cf7a06776f3833a485e0c17f5f7cc82b70947
cho-hyeonjin <gusls2005@gmail.com>
2024-01-24 14:21:38 +0900

    style: VideoDetail 스타일 초안 작성

--- src/components/ChannelInfo.jsx
+++ src/components/ChannelInfo.jsx
@@ -1,9 +1,9 @@
 export default function ChannelInfo({ id, name, title }) {
   return (
-    <>
+    <div className="flex my-4 mb-8 items-center">
       <div>{id}</div>
       <div>{name}</div>
       <div>{title}</div>
-    </>
+    </div>
   );
 }
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -9,20 +9,29 @@ export default function VideoDetail() {
 
   return (
     <>
-      <section>
-        <iframe
-          id="player"
-          type="text/html"
-          width="100%"
-          height="720"
-          src={`http://www.youtube.com/embed/${video.id}`}
-          className="rounded-2xl"
-        />
-        <div>
-          <h2>{title}</h2>
-          <ChannelInfo id={channelId} name={channelTitle} />
-          <pre>{description}</pre>
-        </div>
+      <section className="flex flex-col lg:flex-row">
+        <article className="basis-4/6">
+          <iframe
+            id="player"
+            type="text/html"
+            width="100%"
+            height="720"
+            src={`http://www.youtube.com/embed/${video.id}`}
+            className="rounded-2xl"
+          />
+          <div className="p-8">
+            <h2 className="text-xl font-bold">{title}</h2>
+            <ChannelInfo id={channelId} name={channelTitle} />
+            <pre className="whitespace-pre-wrap">{description}</pre>
+          </div>
+        </article>
+        <section className="basis-2/6">
+          <h1>관련</h1>
+          <h1>비디오</h1>
+          <h1>리스트</h1>
+          <h1>들어올</h1>
+          <h1>예정</h1>
+        </section>
       </section>
     </>
   );
